5601a1723a31f3a4bc7a942b147381c15913411e
yoshihiko.shiraki <shirakiya.pv@gmail.com>
2014-09-23 23:58:02 +0900

    .vim/conf/plugin_conf.vimをdotfilesLinkに追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,10 +1,29 @@
 #! /bin/sh
 
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/zshenv ~/.zshenv
-ln -s ~/dotfiles/zshrc.alias ~/.zshrc.alias
-ln -s ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/bash_profile ~/.bash_profile
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
+DOTDIR=$HOME/dotfiles
+VIMDIR=$HOME/.vim
+VIMCONFDIR=$VIMDIR/conf
+VIMBUNDLEDIR=$VIMDIR/bundle
+
+ln -s $DOTDIR/vimrc $HOME/.vimrc
+if ! [ -e $VIMDIR ]; then
+    mkdir $VIMDIR
+fi
+if [ ! -e $VIMCONFDIR ]; then
+    mkdir $VIMCONFDIR
+fi
+ln -s $DOTDIR/.vim/conf/plugin_conf.vim $VIMCONFDIR/plugin_conf.vim
+
+if [ ! -e $VIMBUNDLEDIR ]; then
+    mkdir $VIMBUNDLEDIR
+    git clone https://github.com/Shougo/neobundle.vim $VIMBUNDLEDIR/neobundle.vim
+    $VIMBUNDLEDIR/neobundle.vim/bin/neoinstall
+fi
+
+ln -s $DOTDIR/zshrc        $HOME/.zshrc
+ln -s $DOTDIR/zshenv       $HOME/.zshenv
+ln -s $DOTDIR/zshrc.alias  $HOME/.zshrc.alias
+ln -s $DOTDIR/bashrc       $HOME/.bashrc
+ln -s $DOTDIR/bash_profile $HOME/.bash_profile
+ln -s $DOTDIR/tmux.conf    $HOME/.tmux.conf
+ln -s $DOTDIR/gitconfig    $HOME/.gitconfig
