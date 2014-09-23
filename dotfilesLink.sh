c38146a090f3377d8599ec9c1f97dafcb7b6b070
yoshihiko.shiraki <shirakiya.pv@gmail.com>
2014-09-23 20:48:43 +0900

    dotfilesを可視化

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.zshenv ~/.zshenv
-ln -s ~/dotfiles/.zshrc.alias ~/.zshrc.alias
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/zshenv ~/.zshenv
+ln -s ~/dotfiles/zshrc.alias ~/.zshrc.alias
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
