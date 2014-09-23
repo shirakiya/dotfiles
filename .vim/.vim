d6a4e0c4bbc64fe77134ff992724a0c9bbc488a3
yoshihiko.shiraki <shirakiya.pv@gmail.com>
2014-09-23 18:37:32 +0900

    色設定の変更

--- .vim/.vim
+++
@@ -1,1 +0,0 @@
-/Users/yoshihikoshiraki/dotfiles/.vim
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,10 +1,9 @@
 #! /bin/sh
 
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zshenv ~/.zshenv
-ln -s ~/dotfiles/.zshenv.alias ~/.zshenv.alias
+ln -s ~/dotfiles/.zshrc.alias ~/.zshrc.alias
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
