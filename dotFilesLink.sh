7cddb9c258a3474b498029e8162288d49cafb9e5
yoshihiko.shiraki <shirakiya.pv@gmail.com>
2015-11-05 10:19:03 +0900

    peco設定ファイル追加

--- dotFilesLink.sh
+++ dotFilesLink.sh
@@ -3,18 +3,20 @@
 DOTDIR=$HOME/dotfiles
 VIMDIR=$HOME/.vim
 VIMBUNDLEDIR=$VIMDIR/bundle
+PECODIR=$HOME/.peco
 
 function make_ln() {
     if [ -e ${HOME}/.${1} ]; then
         echo "\033[31mAlready exist ${HOME}/.${1} as symbolic link.\033[m"
     else
-        echo "\033[32mexec ln -s ${DOTDIR}/${1} ${HOME}/.${1}\033[m"
         ln -s ${DOTDIR}/${1} ${HOME}/.${1}
+        echo "\033[32mexec ln -s ${DOTDIR}/${1} ${HOME}/.${1}\033[m"
     fi
 }
 
 make_ln vimrc
-if ! [ -e $VIMDIR ]; then
+
+if ! [ -e ${VIMDIR} ]; then
     mkdir $VIMDIR
 fi
 
@@ -34,3 +36,11 @@ make_ln tmux.conf
 make_ln gitconfig
 make_ln gitignore
 make_ln my.cnf
+
+
+# for peco setting file
+if [ ! -e ${PECODIR} ]; then
+    mkdir $PECODIR
+    ln -s ${DOTDIR}/scripts/peco_config.json ${PECODIR}/config.json
+    echo "\033[32mexec ln -s ${DOTDIR}/script/peco_config.json ${PECODIR}/.${1}\033[m"
+fi
